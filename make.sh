fcede00e2592bccb2865555366fe130e58120d9c
VieVie31 <orissermaroix@gmail.com>
2017-09-04 01:10:27 +0200

    Adding command to make the bundle file...

---
+++ make.sh
@@ -0,0 +1,6 @@
+mkdir arxiv.bundle
+cp examples.txt arxiv.bundle/examples.txt
+cp info.json arxiv.bundle/info.json
+cp plugin.py arxiv.bundle/plugin.py
+cp template.html arxiv.bundle/template.html
+
